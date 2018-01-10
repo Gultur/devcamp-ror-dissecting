efa7569acf360ae200adda63412ebcab8e9ad870
Gultur <monsieur_yan@hotmail.com>
2018-01-10 02:31:35 +0100

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
+  #get 'pages/home'
+  #get 'pages/about'
+  get 'about-me', to: 'pages#about'
+  #get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # on veut faciliter l'accès au la page d'accueil, il n'y aura plus besoin d'avoir pages/home
+  # mais http://127.0.0.1:3000/ suffit
+  root to: 'pages#home'
+
 end
