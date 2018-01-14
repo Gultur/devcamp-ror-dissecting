8b91b59df0ff442f652117cd5cbeac95956ab88b
Gultur <monsieur_yan@hotmail.com>
2018-01-14 17:08:47 +0100

    integrated custom scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,16 @@ class Portfolio < ApplicationRecord
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  #on peut définir une methode pour selectionner des portfolios
+  # selon un subtitle
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+  #dans le controleur on peut desormais faire Portfolio.angular
+  #
+
+  #une autre façon de faire est de faire un scope
+  #  l'appel dans le controleur sera identique
+  scope :ruby_on_rails_item, ->{where(subtitle: 'Ruby on rails')}
+
 end
