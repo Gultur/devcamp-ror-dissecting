3024e75ef0bfc3e47434c35cbdb9371b9b0f02dc
Gultur <monsieur_yan@hotmail.com>
2018-01-14 15:21:17 +0100

    implemented validations for blogs, skills, portfolios

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,12 @@
 class Blog < ApplicationRecord
+
+  #enum est une liste de valeurs pour un attribut
   enum status: {draft: 0, published: 1}
+
+  #FriendlyId est une gem permettant de creer un slug pour afficher au lieu d'un id
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  # on ne veut pas pouvoir créer/éditer de blg sans titre ou contenu
+  validates_presence_of :title, :body
 end
